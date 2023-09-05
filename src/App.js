131c7b08ac2a598f78de987ae6d699855d53151d
vdemirov <demirov.vlad@gmail.com>
2023-09-05 12:40:56 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class RandomQuoteMachine extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            quote: '',
+            author: ''
+        };
+        this.fetchQuote = this.fetchQuote.bind(this);
+    }
+
+    componentDidMount() {
+        this.fetchQuote();
+    }
+
+    fetchQuote() {
+        fetch('https://api.quotable.io/random')
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    quote: data.content,
+                    author: data.author
+                });
+            })
+            .catch(error => console.error('Error fetching quote:', error));
+    }
+
+    render() {
+        const {quote, author} = this.state;
+        const tweetUrl = `https://twitter.com/intent/tweet?text="${quote}" - ${author}`;
+
+        return (
+            <div className="quote-box" id="quote-box">
+                <div className="quote-text">
+                    <i className="fa fa-quote-left"></i>
+                    <span id="text">{quote}</span>
+                </div>
+                <div className="quote-author">
+                    -
+                    <span id="author"> {author}</span>
+                </div>
+                <div className="buttons">
+                    <a
+                        className="button"
+                        id="tweet-quote"
+                        href={tweetUrl}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <i className="fa fa-twitter"></i>
+                    </a>
+                    <button className="button" id="new-quote" onClick={this.fetchQuote}>New Quote</button>
+                </div>
+            </div>
+        );
+    }
 }
 
-export default App;
+export default RandomQuoteMachine;
